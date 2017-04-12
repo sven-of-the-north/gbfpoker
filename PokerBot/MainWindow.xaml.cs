b1bfbe62d245bd607d13b196d6a166ff1a579fbe
Sven Zhang <zhangss@ucalgary.ca>
2017-04-12 03:44:55 -0600

    Initial commit

--- PokerBot/MainWindow.xaml.cs
+++ PokerBot/MainWindow.xaml.cs
@@ -24,5 +24,30 @@ namespace PokerBot
         {
             InitializeComponent();
         }
+
+        private void StartButton_Click( object sender, RoutedEventArgs e )
+        {
+
+        }
+
+        private void DebugButton_Click( object sender, RoutedEventArgs e )
+        {
+            Point topLeft = Viewport.PointToScreen(new Point(0,0));
+            double width = Viewport.ActualWidth;
+            double height = Viewport.ActualHeight;
+
+            MessageBoxResult msgBox = MessageBox.Show(string.Format("Top left: {0}, Width: {1}, Height: {2}", topLeft.ToString(), width, height));
+        }
+
+        private void ExitButton_Click( object sender, RoutedEventArgs e )
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void MainWindow1_MouseDown( object sender, MouseButtonEventArgs e )
+        {
+            if ( e.ChangedButton == MouseButton.Left )
+                this.DragMove();
+        }
     }
 }
